936f1910a292750192dd0e3a37a7cc255bf77a4e
John Umbriac <43652625+TheRealMothebad@users.noreply.github.com>
2023-02-05 14:22:53 -0500

    added a few comments

--- Scramble_Bot.py
+++ Scramble_Bot.py
@@ -8,21 +8,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from trans_langs import langs
 
+#import token from seperate file to avoid token leaking
 from tok import DontStealMyToken
 
+#start a webdriver (webscraper) with fun and spicy options
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
 g_translate = webdriver.Chrome(options=options)
+
+#the xpath to the google translate text output
 out_box = '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]'
+
+#variables to control bot behavior
 crazyness = 8
 delay = 4
 
 bot = commands.Bot(command_prefix="`")
 
+#generate a url with the text to translate embeded
 async def url_maker(tl, text_to_trans):
     return "https://translate.google.com/?sl=auto&tl="+tl+"&text="+text_to_trans+"&op=translate"
 
+#translate the given sentence to a random lanuage
 async def randomTranslate(ctx, sentence):
     next_l = random.choice(list(langs.keys()))
     await ctx.send("Next: " + langs.get(next_l))
@@ -30,6 +37,8 @@ async def randomTranslate(ctx, sentence):
     time.sleep(delay)
     return g_translate.find_element_by_xpath(out_box).text
 
+#scramble the given message "crazy" amount of times
+#(I promise I have gotten better at naming variables since this was written)
 @bot.command(name="scramble", aliases=['s', 'garble'])
 async def scramble(ctx, *, message):
     "scrambles whatever sentence you put after the command"
@@ -37,7 +46,6 @@ async def scramble(ctx, *, message):
         if x == 1:
             input_text = message
         input_text = await randomTranslate(ctx, input_text)
-        #print(result)
         await ctx.send(input_text)
         if x == crazyness - 1:
             g_translate.get(await url_maker("en", input_text))
